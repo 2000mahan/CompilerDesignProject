9bfbcf337bdfa101e27ed86ec22bb358f4867840
Mahan Ahmadvand <mahan.ah2000@gmail.com>
2020-10-25 15:24:54 +0330

    Add tokens

--- lexer.py
+++ lexer.py
@@ -2,3 +2,11 @@ from ply import lex
 
 class lexer:
 
+    tokens = {
+       'ID', 'INTEGERNUMBER', 'FLOATNUMBER', 'INTEGER', 'FLOAT',
+        'BOOLEAN', 'FUNCTION', 'TRUE', 'FALSE', 'PRINT', 'RETURN',
+        'MAIN', 'IF', 'ELSE', 'ELSEIF', 'WHILE', 'ON', 'WHERE', 'FOR',
+        'AND', 'OR', 'NOT', 'IN', 'ASSIGN', 'SUM', 'MUL', 'DIV',
+        'MOD', 'GT', 'GE', 'LT', 'LE', 'EQ', 'NE', 'LCB', 'RCB',
+        'LRB', 'RRB', 'LSB', 'RSB', 'SEMICOLON', 'COLON', 'COMMA', 'ERROR!'
+    }
