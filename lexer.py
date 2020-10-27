04cb43f6af3fbb706361d37d00ae9d4e8b3ec956
Mahan Ahmadvand <mahan.ah2000@gmail.com>
2020-10-27 20:24:12 +0330

    Some reserved keywords added

--- lexer.py
+++ lexer.py
@@ -2,19 +2,45 @@ from ply import lex
 
 class lexer:
 
-    tokens = {
+    tokens = [
        'ID', 'INTEGERNUMBER', 'FLOATNUMBER', 'INTEGER', 'FLOAT',
         'BOOLEAN', 'FUNCTION', 'TRUE', 'FALSE', 'PRINT', 'RETURN',
         'MAIN', 'IF', 'ELSE', 'ELSEIF', 'WHILE', 'ON', 'WHERE', 'FOR',
         'AND', 'OR', 'NOT', 'IN', 'ASSIGN', 'SUM', 'SUB', 'MUL', 'DIV',
         'MOD', 'GT', 'GE', 'LT', 'LE', 'EQ', 'NE', 'LCB', 'RCB',
         'LRB', 'RRB', 'LSB', 'RSB', 'SEMICOLON', 'COLON', 'COMMA', 'ERROR!'
+    ]
+    reserved = {
+        # Conditional
+        'if': 'IF',
+        'then': 'THEN',
+        'else': 'ELSE',
+        'elseif': 'ELSEIF',
+        # Loops
+        'for': 'FOR',
+        'while': 'WHILE',
+        # Types
+        'int': 'INTEGER',
+        'float': 'FLOAT',
+        'bool': 'BOOLEAN',
+        # Other Keywords
+        'print': 'PRINT',
+        'true': 'TRUE',
+        'false': 'FALSE',
+        'return': 'RETURN',
+        'fun': 'FUNCTION',
+        'main': 'MAIN',
+        'on': 'ON',
+        'and': 'AND',
+        'or': 'OR',
+        'not': 'NOT',
+        'in': 'IN',
     }
-    # COLONS
+    # Colons
     t_SEMICOLON = r';'
     t_COLON = r':'
     t_COLON = r'COMMA'
-    # OPERATORS
+    # Operators
     t_MUL = r'\*'
     t_DIV = r'\/'
     t_SUM = r'\+'
@@ -22,7 +48,7 @@ class lexer:
     t_ASSIGN = r'='
     t_LT = r'\<'
     t_GT = r'\>'
-    # BRACKETS
+    # Brackets
     t_LCB = r'\{'
     t_RCB = r'\}'
     t_LRB = r'\('
