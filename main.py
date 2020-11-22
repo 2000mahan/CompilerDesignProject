b12f95dde9c5d41d0ec4ef89132753e388cc557a
Mohammad MDSA <samimd.77@gmail.com>
2020-11-22 16:48:39 +0330

    Add parser build

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from lexer import Lexer
+from parser import Parser
 
 lexer = Lexer().build()
 file = open('test3.txt')
 text_input = file.read()
 file.close()
 lexer.input(text_input)
-while True:
-    tok = lexer.token()
-    if not tok: break
-    print(tok)
+# while True:
+#     tok = lexer.token()
+#     if not tok: break
+#     print(tok)
+
+parser = Parser()
+parser.build().parse(text_input, lexer, False)
 
 
 
